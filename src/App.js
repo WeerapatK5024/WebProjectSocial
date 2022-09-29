32d97644aad575d49ec5d101d738386ab63f9324
Patcharaporn <patcharaporn_satan@cmu.ac.th>
2022-09-29 16:20:04 +0700

    Initial files

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
-import './app.css';
+import "./app.css";
 
-import { db } from './firebase';
-import { collection, Collections, onSnapshot, snapshotEqual } from 'firebase/firestore';
-import React, { useState, useContext, useEffect } from 'react';
-import ThreadList from './components/ThreadList';
-import Nav from './components/Nav';
-import { ThreadProvider } from './ThreadContext';
-import AddThread from './AddThread';
+import { db } from "./firebase";
+import {
+  collection,
+  Collections,
+  onSnapshot,
+  snapshotEqual,
+} from "firebase/firestore";
+import React, { useState, useContext, useEffect } from "react";
+import ThreadList from "./components/ThreadList";
+import Nav from "./components/Nav";
+import { ThreadProvider } from "./ThreadContext";
+import AddThread from "./AddThread";
+import Signup from "./components/Signup";
 
-import { Add } from '@mui/icons-material';
-import { AuthContextProvider } from './authContext';
-import Login from './components/Login'; 
-
-
+// import { Add } from '@mui/icons-material';
+import { AuthContextProvider } from "./authContext";
+import { Route, Routes } from "react-router-dom";
+import ProtectedRoute from "./components/ProtectedRoute";
+import Account from "./components/Account";
+import { BrowserRouter, Link } from "react-router-dom/dist";
 
 function App() {
   // useEffect(() => {
@@ -22,29 +29,36 @@ function App() {
   // }, [input]);
 
   return (
+    <>
     <AuthContextProvider>
       <ThreadProvider>
         <div>
-
-          <div className='Headder'>
-            <div className='homeText'>
-              HOME / PAGE
-            </div>
+          <div className="Headder">
+            <div className="homeText">HOME / PAGE</div>
           </div>
-
           <div className="mainFrame">
             <div>
               <Nav></Nav>
               <ThreadList />
-
             </div>
             <AddThread />
           </div>
-
         </div>
       </ThreadProvider>
     </AuthContextProvider>
+    <BrowserRouter>
+            <Routes>
+              <Route path="/signup" element={<Signup />} />
 
+              {/* <Route path='/account'element={
+                <ProtectedRoute>
+                  <Account />
+                </ProtectedRoute>
+              } 
+            /> */}
+            </Routes>
+          </BrowserRouter>
+    </>
   );
 }
 
