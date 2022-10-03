db53ebbb29197617267ef321c2bee86a460655ec
WeerapatK5024 <93969220+WeerapatK5024@users.noreply.github.com>
2022-10-03 14:36:33 +0700

    Updated

--- src/index.js
+++ src/index.js
@@ -2,14 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter as Router,Route} from 'react-router-dom';
 import Signup from './components/Signup';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App/>
+    <App>
+      default
+    </App>
   </React.StrictMode>
   );
 
